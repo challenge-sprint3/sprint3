ad7d74dc403c34c1cff9e5f6cca36bc0b2b76d6a
vinxlux <vinx.lux@gmail.com>
2025-10-01 13:28:24 -0300

    Imagens adicionadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './Home'
-import './global.css'
+import './styles/App.css'
+import Home from './pages/Home'
+import './styles/global.css'
 
 function App() {
   const [count, setCount] = useState(0)
